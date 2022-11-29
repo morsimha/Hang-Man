092796bdb13df695885ae1d2ec4f659fafb6a2dd
morsimha <mor.simha@intel.com>
2022-11-29 19:07:38 +0200

    added Joption

--- src/main/java/com/example/hangman/HangManController.java
+++ src/main/java/com/example/hangman/HangManController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import javax.swing.*;
 import java.io.FileNotFoundException;
 
 public class HangManController {
@@ -25,14 +26,14 @@ public class HangManController {
     private Button btnGuess;
 
 
-    private final int SIZE = 200;
-    Words first = new Words();
+    private final int TRIES = 6;
+    Words dict = new Words();
     HangManLogic game = new HangManLogic();
 
     public void initialize() throws FileNotFoundException {
-        labelTries.setText("6");
-        first.ReadWords();
-        game.word = first.generateWord(); // maybe private and get\set
+        labelTries.setText("" + TRIES);
+        dict.ReadWords();
+        game.word = dict.generateWord(); // maybe private and get\set
         game.prepWordLength(game.word.length());
       //  System.out.println(game.word);
         labelWord.setText(game.currGuess);
@@ -41,29 +42,29 @@ public class HangManController {
 
     @FXML
     private void btnPressed(){
-        if (game.triesLeft != 0) {
-            labelTries.setText("" + game.triesLeft);
-            String guess = guessField.getText();
-            game.guesses.add(guess);
-            //    game.getGuess();
-            game.checkGuess();
-            System.out.println(game.currGuess);
-            labelWord.setText(game.currGuess);
-            guessField.clear();
-            labelLetters.setText(game.guesses.toString());
-        }
+        if (game.triesLeft == 0)
+            JOptionPane.showMessageDialog(null, "Game Over!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
         else
-            System.out.println("Game over");
+        {
+            String guess = guessField.getText();
+            if (guess.length() == 1)
+                runGame(guess);
+            else
+                JOptionPane.showMessageDialog(null, "Please enter one character only!", "Error", JOptionPane.ERROR_MESSAGE);
+            guessField.clear();
+        }
+    }
+
+    private void runGame(String guess){
+        labelTries.setText("" + game.triesLeft);
+        game.guesses.add(guess);
+        game.checkGuess();
+        labelWord.setText(game.currGuess);
+        labelLetters.setText(game.guesses.toString());
     }
 
 
-    public void drawHangMan(){
-
-
-        System.out.println("Welcome to hangman, too lazy for fx:");
-        System.out.println("This is your very predictable word to guess:");
-        System.out.println(game.currGuess);
-
+    private void drawHangMan(){
 
 //        while (!game.currGuess.equals(game.word)) {
 //
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.hangman {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.hangman to javafx.fxml;
